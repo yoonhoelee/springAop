08b354749bd4db7a911d28dc904f41ad01613f5b
yoonhoelee <ubrain0624@gmail.com>
2022-03-19 15:09:05 +0900

    chapter 1 practice

---
+++ src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,27 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.hellotrace.HelloTraceV1;
+import hello.advanced.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) { trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+}
