7ce59124f9c0bfa7843861623d0e25a8b962a9f0
yoonhoelee <ubrain0624@gmail.com>
2022-05-07 17:16:23 +0900

    with threadlocal

--- src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -5,32 +5,25 @@ import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ThreadLocalLogTrace implements LogTrace{
+public class ThreadLocalLogTrace implements LogTrace {
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
     private ThreadLocal<TraceId> traceIdHolder = new ThreadLocal<>();
-
     @Override
     public TraceStatus begin(String message) {
         syncTraceId();
         TraceId traceId = traceIdHolder.get();
         Long startTimeMs = System.currentTimeMillis();
-        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX,
+                traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
-    }
-    private void syncTraceId(){
-        TraceId traceId = traceIdHolder.get();
-        if(traceIdHolder==null){
-            traceIdHolder.set(new TraceId());
-        }else{
-            traceIdHolder.set(traceId.createNextId());
-        }
     }
     @Override
     public void end(TraceStatus status) {
         complete(status, null);
     }
+    @Override
     public void exception(TraceStatus status, Exception e) {
         complete(status, e);
     }
@@ -49,16 +42,22 @@ public class ThreadLocalLogTrace implements LogTrace{
         }
         releaseTraceId();
     }
-
+    private void syncTraceId() {
+        TraceId traceId = traceIdHolder.get();
+        if (traceId == null) {
+            traceIdHolder.set(new TraceId());
+        } else {
+            traceIdHolder.set(traceId.createNextId());
+        }
+    }
     private void releaseTraceId() {
         TraceId traceId = traceIdHolder.get();
-        if(traceId.isFirstLevel()){
-            traceIdHolder.remove();
-        }else{
+        if (traceId.isFirstLevel()) {
+            traceIdHolder.remove();//destroy
+        } else {
             traceIdHolder.set(traceId.createPreviousId());
         }
     }
-
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < level; i++) {
---
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,56 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+@Slf4j
+public class TemplateMethodTest {
+    @Test
+    void templateMethodV0(){
+        logic1();
+        logic2();
+    }
+    private void logic1(){
+        long startTime = System.currentTimeMillis();
+        log.info("비즈니스 로직1 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+    private void logic2(){
+        long startTime = System.currentTimeMillis();
+        log.info("비즈니스 로직2 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+    @Test
+    void templateMethodV1(){
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+    @Test
+    void templateMethodV2(){
+        AbstractTemplate template1 = new AbstractTemplate(){
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        };
+        template1.execute();
+        AbstractTemplate template2 = new AbstractTemplate(){
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        };
+        template2.execute();
+    }
+
+
+}
